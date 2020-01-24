2cb653e0104511e8b50ad3c76e1fb8a6b3423f88
Soel Karki <karkisoel@gmail.com>
2020-01-24 18:33:59 +0545

    progress in design

--- HospitalReportAndroid/app/src/main/java/com/example/hospitalreport/MainActivity.java
+++ HospitalReportAndroid/app/src/main/java/com/example/hospitalreport/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.hospitalreport;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.hospitalreport.hospital.HospitalLoginActivity;
+import com.example.hospitalreport.patient.PatientLoginActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.hospital_card)
+    CardView hospital;
+
+    @BindView(R.id.patient_card)
+    CardView patient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
     }
+
+    @OnClick(R.id.hospital_card)
+    public void openHospitalLoginRegister()
+    {
+        Intent intent=new Intent(MainActivity.this, HospitalLoginActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.patient_card)
+    public  void  openPatientLoginRegister()
+    {
+        Intent intent=new Intent(MainActivity.this, PatientLoginActivity.class);
+        startActivity(intent);
+    }
+
 }
